31a7174d75668b18b91d8d1f328f115d330a921b
csw <hibogo789@gmail.com>
2021-12-03 11:13:10 +0900

    javascript reduce 이해

--- JavaScript/문제 해결 접근법/문제의 이해.js
+++ JavaScript/문제 해결 접근법/문제의 이해.js
@@ -15,8 +15,39 @@ function add(num1, num2) {
   return sum;
 }
 
+function charCount(q) {
+  // let sample = q.split("");
+
+  let sample = q.split("").sort();
+
+  const count = sample.reduce((acc, current) => {
+    if (acc[current]) {
+      acc[current] += 1;
+    } else {
+      acc[current] = 1;
+    }
+    return acc;
+  }, {});
+  console.log(count);
+}
+
+function avgNum(numArray) {
+  const avg = numArray.reduce((acc, current, index, array) => {
+    // index 의 갯수가 배열 갯수와 같아졌을 경우엔 스탑
+    if (index === array.length - 1) {
+      return (acc + current) / array.length;
+    }
+    // if 문 통과하기 전까지 모든 숫자를 더한다.
+    return acc + current;
+  }, 0);
+  console.log(avg);
+}
+
+num = [1, 2, 3];
+avgNum(num);
+
 // 먼저 알 수 있는 것은 출력값이 배열이나 다른 데이터 구조가 아니라 객체여야 한다.
-charCount("aaaa"); // 출력 => { a:4 }
+// charCount("aaaa"); // 출력 => { a:4 }
 // 출력값에는 입력값에 있는 글자만 보여주면 되는 건가? 만약 입력값에 없는 글자는?
 // { b:0, c:0 } 이런식으로 추가해야 하나?
 charCount("hello"); // 출력 => { h:1, e:1, l:2, o:1 }
@@ -24,16 +55,16 @@ charCount("hello"); // 출력 => { h:1, e:1, l:2, o:1 }
 // 이것이 입력값이라면 출력값은 어떻게 되어야 하나?
 // => 띄어쓰기도 고려해야 하나?
 // 달러 표시나 밑줄이나 숫자같은 다른 문자들은 어떻게 처리??
-("내 전화번호는 1111002");
+// charCount("내 전화번호는 1111002");
 
 // 대문자 H 와 소문자 h 가 있으면 대소문자 구분을 해야 하는가?
-("Hello hi");
+// charCount("Hello hi");
 
 // 이런식으로 복잡한 입력값을 떠올릴 수록 문제를 이해하는데 도움이 되고 이런 질문들을 하면서 문제를 단순화 해서 더 깊이 이해할 수 있다.
 // 문제에 달려들기 전에 문제를 이해하는 다른 방식이다.
 
 // 이런식으로 charCount 에 아무것도 넣지 않거나 비어있는 문자열을 입력하면 어떻게 되나?
-charCount(); // null false undefined error 중에 무엇을 출력해야 할까?
+// charCount(); // null false undefined error 중에 무엇을 출력해야 할까?
 
 // 문자열이 아니라 숫자나 객체나 null 을 입력하면 어떻게 될까??
 // 물음표 살인마가 되야 한다.
