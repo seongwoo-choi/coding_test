9237a19b91df12f5c9fb1d3c40328f5fda557729
csw <hibogo789@gmail.com>
2021-12-03 14:33:09 +0900

    charCount 함수 수정

--- JavaScript/문제 해결 접근법/문제의 이해.js
+++ JavaScript/문제 해결 접근법/문제의 이해.js
@@ -16,15 +16,13 @@ function add(num1, num2) {
 }
 
 function charCount(q) {
-  // let sample = q.split("");
+  let sample = q.split("").sort().join("").trim().split("");
 
-  let sample = q.split("").sort();
-
-  const count = sample.reduce((acc, current) => {
-    if (acc[current]) {
-      acc[current] += 1;
+  const count = sample.reduce((acc, cur, index, arr) => {
+    if (acc[cur]) {
+      acc[cur] += 1;
     } else {
-      acc[current] = 1;
+      acc[cur] = 1;
     }
     return acc;
   }, {});
@@ -47,7 +45,7 @@ num = [1, 2, 3];
 avgNum(num);
 
 // 먼저 알 수 있는 것은 출력값이 배열이나 다른 데이터 구조가 아니라 객체여야 한다.
-// charCount("aaaa"); // 출력 => { a:4 }
+charCount("aaaa"); // 출력 => { a:4 }
 // 출력값에는 입력값에 있는 글자만 보여주면 되는 건가? 만약 입력값에 없는 글자는?
 // { b:0, c:0 } 이런식으로 추가해야 하나?
 charCount("hello"); // 출력 => { h:1, e:1, l:2, o:1 }
@@ -55,10 +53,10 @@ charCount("hello"); // 출력 => { h:1, e:1, l:2, o:1 }
 // 이것이 입력값이라면 출력값은 어떻게 되어야 하나?
 // => 띄어쓰기도 고려해야 하나?
 // 달러 표시나 밑줄이나 숫자같은 다른 문자들은 어떻게 처리??
-// charCount("내 전화번호는 1111002");
+charCount("내 전화번호는 1111002");
 
 // 대문자 H 와 소문자 h 가 있으면 대소문자 구분을 해야 하는가?
-// charCount("Hello hi");
+charCount("Hello hi");
 
 // 이런식으로 복잡한 입력값을 떠올릴 수록 문제를 이해하는데 도움이 되고 이런 질문들을 하면서 문제를 단순화 해서 더 깊이 이해할 수 있다.
 // 문제에 달려들기 전에 문제를 이해하는 다른 방식이다.
